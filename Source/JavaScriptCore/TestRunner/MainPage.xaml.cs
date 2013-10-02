ba4b8514f0dc7e82f6f2ba13a3ce8093e22daab4
Martin Robinson <mrobinson@igalia.com>
2013-10-01 18:56:29 -0700

    Add a test harness for running the test262 tests

---
+++ Source/JavaScriptCore/TestRunner/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TestRunner.Resources;
+using Windows.Storage;
+using System.Threading.Tasks;
+using System.IO;
+using TestRunnerComponent;
+using System.Collections.ObjectModel;
+
+namespace TestRunner
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        TestRunnerComponent.ScriptRunner scriptRunner;
+        bool runningTests = false;
+        ObservableCollection<TestRunnerComponent.ScriptResult> failedTests = new ObservableCollection<TestRunnerComponent.ScriptResult>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.scriptRunner = new TestRunnerComponent.ScriptRunner();
+            FailedTestsListSelector.ItemsSource = failedTests;
+        }
+
+        private static async Task<String> LoadTestHarnessScripts()
+        {
+            String cth = await LoadHarnessFile("cth.js");
+            String sta = await LoadHarnessFile("sta.js");
+            String ed = await LoadHarnessFile("ed.js");
+            String testBuiltinObject = await LoadHarnessFile("testBuiltInObject.js");
+            String testIntl = await LoadHarnessFile("testIntl.js");
+            return cth + sta + ed + testBuiltinObject + testIntl;
+        }
+
+        private static async Task<String> LoadFileFromInstallationDirectorySubdirectory(string subdirectory, string filePath)
+        {
+            filePath = Windows.ApplicationModel.Package.Current.InstalledLocation.Path + "\\" + subdirectory + "\\" + filePath;
+            StorageFile scriptFile = await StorageFile.GetFileFromPathAsync(filePath);
+            Stream fileStream = await scriptFile.OpenStreamForReadAsync();
+            using (StreamReader streamReader = new StreamReader(fileStream))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+
+        private static async Task<String> LoadSuiteFile(string filePath)
+        {
+            return await LoadFileFromInstallationDirectorySubdirectory("suite", filePath);
+        }
+
+        private static async Task<String> LoadHarnessFile(string filePath)
+        {
+            return await LoadFileFromInstallationDirectorySubdirectory("harness", filePath);
+        }
+
+        private async Task<String[]> GetTestPaths()
+        {
+            String testList = await LoadSuiteFile("script-files.txt");
+            return testList.Split('\n').Where(t => t.Length > 0 && t[0] != '#').ToArray();
+        }
+
+        private async void StartTestsCallback(object sender, RoutedEventArgs e)
+        {
+            if (runningTests == true)
+                return;
+
+            runningTests = true;
+            String[] testPaths = await GetTestPaths();
+            String harnessString = await LoadTestHarnessScripts();
+
+            StatusText.Text = "Found " + testPaths.Count() + " tests";
+            Progress.Minimum = 0;
+            Progress.Maximum = 200;
+            Progress.Value = 0;
+            failedTests.Clear();
+
+            for (int i = 0; i < Progress.Maximum; i++)
+            {
+                String script = await LoadSuiteFile(testPaths[i]);
+                String fullScript = harnessString + script;
+                TestRunnerComponent.ScriptResult result = this.scriptRunner.RunScript(fullScript, testPaths[i]);
+
+                StatusText.Text = "Ran " + (i + 1) + " of " + Progress.Maximum + " tests (" + failedTests.Count + " failed)";
+                System.Diagnostics.Debug.WriteLine("Test " + testPaths[i] + "(" + i + ")" + " passed: " + result.Success());
+
+                // FIXME: This likely deserves a better check.
+                bool positiveTest = !script.Contains("@negative");
+                if (positiveTest != result.Success())
+                    failedTests.Add(result);
+
+                Progress.Value = i;
+            }
+        }
+
+        private void FailedTestClickedCallback(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (FailedTestsListSelector.SelectedItem is TestRunnerComponent.ScriptResult)
+            {
+                TestRunnerComponent.ScriptResult result = (TestRunnerComponent.ScriptResult)FailedTestsListSelector.SelectedItem;
+                MessageBox.Show(result.TestPath() + "\n\n" + result.ExceptionString());
+            }
+        }
+    }
+}
---
+++ Source/JavaScriptCore/TestRunnerComponent/TestRunnerComponent.cpp
@@ -0,0 +1,46 @@
+﻿// TestRunnerComponent.cpp
+
+#include "TestRunnerComponent.h"
+#include <string>
+#include <windows.h>
+
+using namespace TestRunnerComponent;
+using namespace Platform;
+
+extern int jscmainRepeatable(const std::string& script, const std::string& fileName, std::string& exceptionString);
+
+ScriptRunner::ScriptRunner()
+{
+}
+
+std::string convertPlatformStringtoUTF8(Platform::String^ in)
+{
+    size_t sizeRequired = WideCharToMultiByte(CP_UTF8, 0, in->Data(), in->Length(), NULL, 0, NULL, NULL);
+    std::string result(sizeRequired, 0);
+    WideCharToMultiByte(CP_UTF8, 0, in->Data(), in->Length(), &result[0], sizeRequired, NULL, NULL);
+    return result;
+}
+
+Platform::String^ convertUTF8ToPlatformString(std::string utf8)
+{
+    size_t sizeRequired = MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, NULL, 0);
+    std::wstring result(sizeRequired, 0);
+    MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, &result[0], sizeRequired);
+    return ref new Platform::String(result.c_str());
+}
+
+ScriptResult^ ScriptRunner::RunScript(Platform::String^ script, Platform::String^ fileName)
+{
+    std::string scriptString = convertPlatformStringtoUTF8(script);
+    std::string fileNameString = convertPlatformStringtoUTF8(fileName);
+    std::string exceptionString;
+    bool success = !jscmainRepeatable(scriptString.c_str(), fileNameString.c_str(), exceptionString);
+    return ref new ScriptResult(success, fileName, convertUTF8ToPlatformString(exceptionString));
+}
+
+ScriptResult::ScriptResult(bool success, Platform::String^ testPath, Platform::String^ exceptionString)
+    : m_success(success)
+    , m_testPath(testPath)
+    , m_exceptionString(exceptionString)
+{
+}
